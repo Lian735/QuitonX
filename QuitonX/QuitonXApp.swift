47ff8f7fee64ac2cba28899fd1cb7833980f8a19
Lian <176339768+Lian735@users.noreply.github.com>
2025-12-21 12:47:05 +0100

    Initial Commit

---
+++ QuitonX/QuitonXApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitonXApp.swift
+//  QuitonX
+//
+//  Created by Lian on 21.12.25.
+//
+
+import SwiftUI
+
+@main
+struct QuitonXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
